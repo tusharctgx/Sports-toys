a893855952152c5d52839de2bf58ea077907190c
tusharctgx <tusharctg671@gmail.com>
2023-05-21 16:52:30 +0600

    banner add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,18 +1,18 @@
-// import Banner from "../Banner/Banner";
-// import Category from "../Category/Category";
-// import Gallary from "../Gallary/Gallary";
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import Gallary from "../Gallary/Gallary";
 
 
-// const Home = () => {
-//   return (
-//     <div>
-//       <Banner></Banner>
-//       <Gallary></Gallary>
-//       <br />
-//       <Category></Category>
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Gallary></Gallary>
+      <br />
+      <Category></Category>
       
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
-// export default Home;
+export default Home;
